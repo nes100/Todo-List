6b2c14fd94c08092131c5064726fdc25f48507c0
Anes <suhonjicanes96@gmail.com>
2023-03-08 21:46:13 -0600

    Implement checkboxes and date

--- Web.py
+++ Web.py
@@ -9,13 +9,18 @@ def add_todo():
     functions.write(todos)
 
 st.title("My Todo App ")
-st.subheader("This is my todo app")
+st.subheader("This is my todo list for today")
+st.date_input("Date")
 st.write("This app is to increase your productivity")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+         todos.pop(index)
+         functions.write(todos)
+         del st.session_state[todo]
+         st.experimental_rerun()
 
 st.text_input(label=" ",placeholder="Add a todo...",
               on_change=add_todo, key="new_todo")
 
-st.session_state
